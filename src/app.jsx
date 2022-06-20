1775662b4f1f017a65559ecbe2f1eccba3ed4499
Kwon Ena <eeena98@naver.com>
2022-06-20 23:12:30 +0900

    Style : AwardsList component publishing

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,35 @@
+import styled from "styled-components";
 import AwardsList from "./components/awardsList";
 import CountList from "./components/countList";
 import LogoImage from "./components/logoImage";
 
 function App() {
   return (
-    <>
-      <LogoImage />
-      <CountList />
-      <AwardsList />
-    </>
+    <AppStyle>
+      <Section1>
+        <LogoImage />
+      </Section1>
+      <Section2>
+        <CountList />
+        <AwardsList />
+      </Section2>
+    </AppStyle>
   );
 }
+
+const AppStyle = styled.section`
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Section1 = styled.section``;
+const Section2 = styled(Section1)`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default App;
--- src/components/awardsList.jsx
+++ src/components/awardsList.jsx
@@ -6,21 +6,46 @@ const APPSTORE_IMAGE = "/images/badge-apple4x.png";
 
 const AwardsList = memo((props) => {
   return (
-    <>
-      <GoogleImg src={process.env.PUBLIC_URL + PLAYSTORE_IMAGE} alt="logo" />;
-      <AppleImg src={process.env.PUBLIC_URL + APPSTORE_IMAGE} alt="logo" />;
-    </>
+    <Container>
+      <AwardsItem>
+        <ItemImg src={process.env.PUBLIC_URL + PLAYSTORE_IMAGE} alt="logo" />
+        <ItemText>
+          2018 구글 플레이스토어
+          <br />
+          올해의 앱 최우수상 수상
+        </ItemText>
+      </AwardsItem>
+      <AwardsItem>
+        <ItemImg src={process.env.PUBLIC_URL + APPSTORE_IMAGE} alt="logo" />
+        <ItemText>
+          2018 애플 앱스토어
+          <br />
+          올해의 여행앱 선정
+        </ItemText>
+      </AwardsItem>
+    </Container>
   );
 });
 
-const GoogleImg = styled.img`
-  width: 400px;
-  height: 338px;
+const Container = styled.section`
+  display: flex;
 `;
 
-const AppleImg = styled(GoogleImg)`
-  width: 400px;
-  height: 338px;
+const AwardsItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 50px;
+`;
+
+const ItemImg = styled.img`
+  width: 54px;
+  height: 54px;
+  margin-right: 10px;
+`;
+
+const ItemText = styled.span`
+  color: rgba(58, 58, 58, 0.8);
+  font-weight: bold;
 `;
 
 export default AwardsList;
